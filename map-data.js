f39436b1dead4deeccf36a4b1607472bcdcf1fc2
Allen Ponce de Leon <allen@DESKTOP-90GUCIQ.localdomain>
2021-11-28 18:06:20 +0800

    added way for drawing svg elements on a png map in the popup

--- map-data.js
+++ map-data.js
@@ -4,7 +4,7 @@ let map_state = [
       icon: 'triangle.png',
       coordinate: {
       x: 320,
-          y: 120	
+      y: 120	
        },
        pest_found: [
       {
@@ -20,13 +20,39 @@ let map_state = [
   ]
 
 
+// Load map data
+
+
+
+
+// helper function for setting multiple attributes at once on an element
+function setAttributes(el, attrs) {
+  for (let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
 // Open the Modal
 function openModal() {
     document.getElementById("mapModal").style.display = "block";
+    
 }
+
 
 // Close the modal
 
 function closeModal() {
     document.getElementById("mapModal").style.display = "none";
-  }
+}
+
+
+function mapLoad(){
+  let drawMap = document.getElementById('drawMap');
+  let mapElement = document.createElementNS('http://www.w3.org/2000/svg','circle');
+
+  setAttributes(mapElement, {'cx': map_state[0].coordinate.x, 'cy': map_state[0].coordinate.y, 'r': '20', 'stroke': '#000000', 'stroke-width': '3', 'fill': 'red', 'data-bs-toggle':'tooltip', 'data-bs-placement':'bottom', 'title':map_state[0].pest_found[0].pest_name});
+
+
+  drawMap.appendChild(mapElement);
+
+}
