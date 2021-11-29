a4442ee5c6cb21cec234fd60bcf67ae7e5715e2d
Allen Ponce de Leon <allen@DESKTOP-90GUCIQ.localdomain>
2021-11-29 11:55:34 +0800

    Added comments to map-data.js

--- map-data.js
+++ map-data.js
@@ -40,13 +40,16 @@ function closeModal() {
 
 // Load map data
 function mapLoad(){
+  // Get the map area to be drawn to
   let drawMap = document.getElementById('drawMap');
+
+  // create the svg element to be added
   let mapElement = document.createElementNS('http://www.w3.org/2000/svg','circle');
 
-
+  // use helper function to set attributes of created svg element from data passed
   setAttributes(mapElement, {'cx': map_state[0].coordinate.x, 'cy': map_state[0].coordinate.y, 'data-bs-toggle':'tooltip', 'data-bs-placement':'bottom', 'title':map_state[0].pest_found[0].pest_name});
 
-
+  // add the element to the map area on the DOM
   drawMap.appendChild(mapElement);
 
 }
