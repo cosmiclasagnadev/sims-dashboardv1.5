1b39c72bfb3b80c88ad0c98bf4d89b2ba1f8f718
Allen Ponce de Leon <allen@DESKTOP-90GUCIQ.localdomain>
2021-11-29 10:57:49 +0800

    Continued map functionality

--- map-data.js
+++ map-data.js
@@ -41,13 +41,17 @@ function closeModal() {
 // Load map data
 function mapLoad(){
   let drawMap = document.getElementById('drawMap');
-
-
   let mapElement = document.createElementNS('http://www.w3.org/2000/svg','circle');
 
-  setAttributes(mapElement, {'cx': map_state[0].coordinate.x, 'cy': map_state[0].coordinate.y, 'r': '20', 'stroke': '#000000', 'stroke-width': '3', 'fill': 'red', 'data-bs-toggle':'tooltip', 'data-bs-placement':'bottom', 'title':map_state[0].pest_found[0].pest_name});
+  let svgElement = '<path id="dp_path001" cx="320" cy="120" r="20" fill="none" fill-rule="evenodd" stroke="black" stroke-width="4" d="M 380.34,401.76 C 385.624,401.76 389.908,406.043 389.908,411.327 C 389.908,416.611 385.624,420.893 380.34,420.893 C 375.056,420.893 370.772,416.611 370.772,411.327 C 370.772,406.043 375.056,401.76 380.34,401.76 Z "/>'
+  let domElement = document.createElement('div');
+
+  domElement.innerHTML = svgElement;
 
 
-  drawMap.appendChild(mapElement);
+  setAttributes(mapElement, {'cx': map_state[0].coordinate.x, 'cy': map_state[0].coordinate.y, 'data-bs-toggle':'tooltip', 'data-bs-placement':'bottom', 'title':map_state[0].pest_found[0].pest_name});
+
+
+  drawMap.appendChild(domElement.firstChild);
 
 }
